77f68268688596d778e00a709e8ad4f8f870c031
Vinay Bharadhwaj <vinaysbharadhwaj@gmail.com>
2019-02-13 19:05:19 +0100

    Scrapes the Wallpapers subreddit for images

--- RedditScrapper.py
+++ RedditScrapper.py
@@ -1,12 +1,40 @@
 import praw
+import re
+import requests
+import os
 
-reddit = praw.Reddit(client_id='CLIENT_ID',
-                     client_secret="CLIENT_SECRET", password='PASSWORD',
-                     user_agent='USERAGENT', username='USERNAME')
 
-subreddit = reddit.subreddit('reddit_api_test')
+def Scrapper(subredditname='wallpapers'):
+    reddit = praw.Reddit(client_id='szAEvLMd-fUyIQ',
+                         client_secret="RUbLmQVrDRVQ3UvxWtRHAFgPyGg", password='horse@1419',
+                         user_agent='testscript by /u/ByakuyaV', username='ByakuyaV')
 
-for idx, submission in enumerate(subreddit.top('week')):
-    print(submission)
-    if(idx == 4):
-        break
+    subreddit = reddit.subreddit(subredditname)
+    img_count = 0
+    errors = 0
+    for submission in subreddit.top('week'):
+        if(errors >= 50):
+            return(0)  # 'There is not many jpg, jpeg or png image in this subreddit, Choose another subreddit'
+
+        if(img_count == 7):
+            return(1)
+
+        url = (submission.url)
+
+        if not any(x in url for x in ['.jpg', '.png', '.jpeg']):  # Check if there are any images in the subreddit
+            errors += 1
+            continue
+
+        file_name = url.split("/")
+        if len(file_name) == 0:
+            file_name = re.findall("/(.*?)", url)
+        file_name = file_name[-1]
+        if "." not in file_name:
+            file_name += ".jpg"  # Create the image file
+
+        r = requests.get(url)
+        os.makedirs(subredditname + '_Temp_Images', exist_ok=True)
+        with open(subredditname + '_Temp_Images/' + file_name, "wb") as f:
+            f.write(r.content)  # Fill the image file
+
+        img_count += 1
